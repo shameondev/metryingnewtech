dd03647df9bf4c922e1650d34af8191acdb80093
Daniil Fadeev <shameondev@gmail.com>
2022-09-18 22:19:35 +0300

    Added react and css loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,15 @@ import webpack from "webpack";
 
 export const buildLoaders = (): webpack.RuleSetRule[] => {
 
+    const cssLoaders = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader",
+            ],
+        };
+
     const typesriptLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',
@@ -9,6 +18,7 @@ export const buildLoaders = (): webpack.RuleSetRule[] => {
         };
 
     return [
-        typesriptLoader
+        typesriptLoader,
+        cssLoaders
     ]
 };
