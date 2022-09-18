087089d1b9ddd23721a80b1b98d52a7014861c9e
Daniil Fadeev <shameondev@gmail.com>
2022-09-18 22:54:39 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Counter} from "./components/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import {MainPage} from "./pages/MainPage/MainPage";
+import {AboutPage} from "./pages/AboutPage/AboutPage";
 
 export const App = () => {
     return (
         <div className="app">
-            Hello world
-            <Counter/>
+            <Link to="/">Главная</Link>
+            <Link to="/about">О сайте</Link>
+            <Routes>
+               <Route path="/" element={<MainPage/>} />
+               <Route path="/about" element={<AboutPage/>} />
+            </Routes>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const MainPage = () => {
+    return (
+        <h1>Main Page</h1>
+    );
+};
