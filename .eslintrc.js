c5c880b929dc949c49895be204f1d75b6f809189
Daniil Fadeev <shameondev@gmail.com>
2022-10-04 19:51:07 +0300

    Small improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,9 +43,21 @@ module.exports = {
         'max-len': ['error', { ignoreComments: true, code: 100 }],
 
         // i18n
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string':
+            [
+                'error',
+                { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
+            ],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,17 +2,32 @@ import { cn } from 'shared/lib/classNames';
 import { AppLink } from 'shared/ui/AppLink';
 
 import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import s from './Navbar.module.scss';
 
 type NavbarProps = {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={cn(s.Navbar, {}, [className])}>
-        <div className={s.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} className={s.mainLink} to="/">Main</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">About</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={cn(s.Navbar, {}, [className])}>
+            <div className={s.links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    className={s.mainLink}
+                    to="/"
+                >
+                    {t('Главная страница')}
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/about"
+                >
+                    {t('О сайте')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
